fab50a469de6439853d031b33eef0cd5e0bc2e08
Robert Wittman <robkwittman@gmail.com>
2023-12-15 10:48:22 -0500

    feat(minecraft): Add Minecraft server types

--- cmd/main.go
+++ cmd/main.go
@@ -96,6 +96,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Valheim")
 		os.Exit(1)
 	}
+	if err = (&controller.MinecraftReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Minecraft")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
